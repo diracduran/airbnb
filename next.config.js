014cc7a53b71f435c4caa661a82a6f9bb103d7e8
dirac <78232235+diracduran@users.noreply.github.com>
2022-04-13 12:24:40 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
         domains: ['links.papareact.com']
     },
     env: {
-        mapbox_key: 'pk.eyJ1IjoiZGlyYWNkdXJhbiIsImEiOiJja3Q1c3Fjc3AwYnM1MnBvN29la2RpOHhoIn0.SiKQ97W-vgcNdN7PkqilXw'
+        mapbox_key: 'mapbox_key'
     }
 }
